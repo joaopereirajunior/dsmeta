2099468f22f16ea4c64c0d9fe9622f2497341423
Joao Junior <Developer@MacBook-Pro-de-Mac.local>
2022-07-13 22:26:34 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-
+import NotificatinButton from "./components/notificationButton";
 
 function App() {
-
-
   return (
- <h1>Olá mundo!</h1>
-  )
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificatinButton />
+    </>
+  );
 }
 
-export default App
+export default App;
