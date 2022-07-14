ee16829df33b96a180f4f71ef43df8ee7ba71513
Joao Junior <Developer@MacBook-Pro-de-Mac.local>
2022-07-14 13:41:20 -0300

    other component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/header";
 import NotificatinButton from "./components/notificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
-      <NotificatinButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
